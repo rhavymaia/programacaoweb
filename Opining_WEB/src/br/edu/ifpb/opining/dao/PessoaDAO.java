887d12b77ecc485272f261fdd04793accb96fa76
rhavymaia <rmg3@cin.ufpe.br>
2015-11-30 12:20:26 -0300

    P. Web
    Refatoring de projeto

---
+++ Opining_WEB/src/br/edu/ifpb/opining/dao/PessoaDAO.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb.opining.dao;
+
+public class PessoaDAO {
+
+}
---
+++ Opining_WEB/src/br/edu/ifpb/opining/entidades/Pessoa.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.opining.entidades;
+
+public class Pessoa {
+
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
