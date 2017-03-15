91dd2eb9ec02e5609e8df4d93e2e663cf26126d4
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2017-03-15 14:47:31 -0300

    Comentários no controller de Aluno
    - Melhorias nos comentário do controller de aluno.

--- RestEasyApp/src/br/edu/ifpb/resteasyapp/controller/AlunoController.java
+++ RestEasyApp/src/br/edu/ifpb/resteasyapp/controller/AlunoController.java
@@ -21,6 +21,12 @@ import br.edu.ifpb.resteasyapp.entidade.Aluno;
 @Path("aluno")
 public class AlunoController {
 
+	/**
+	 * Cadastra o aluno no sistema.
+	 * 
+	 * @param aluno
+	 * @return Response
+	 */
 	@PermitAll
 	@POST
 	@Path("/inserir")
@@ -28,22 +34,35 @@ public class AlunoController {
 	@Produces("application/json")
 	public Response insert(Aluno aluno) {
 		
+		// Preparando a resposta. Provisoriamente o sistema preparará a resposta como requisição incorreta.
 		ResponseBuilder builder = Response.status(Response.Status.BAD_REQUEST);
 		builder.expires(new Date());
 		
+		//TODO: Regra de negócio e manipulação de dados nesse ponto. As informaçãos devem ser associadas
+		// nesse ponto ao biuld (response).
+		
+		// Resposta.
 		return builder.build();
 	}
 	
+	/**
+	 * Retorna todos os alunos cadastrados.
+	 * 
+	 * @return Response
+	 */
 	@PermitAll
 	@GET
 	@Path("/listar")
 	@Produces("application/json")
 	public List<Aluno> getAll() {
 		
+		// Retorno em formato de lista.
+		// Desse modo o response sempre conterá o código de resposta OK.
 		List<Aluno> alunos = new ArrayList<Aluno>();
 		
 		try {
 			
+			//TODO: Regra de negócio e manipulação de dados nesse ponto.
 			alunos = AlunoDAO.getInstance().getAll();
 		
 		} catch (SQLException e) {
@@ -51,29 +70,40 @@ public class AlunoController {
 			// TODO: Tratar a exceção.
 		}
 		
+		// Será retornado ao cliente um conjunto de alunos no formato de Json.
 		return alunos;
 	}
 	
+	/**
+	 * Recupera o alunos cadastrado no sistema através do seu id.
+	 * 
+	 * @param idAluno
+	 * @return Response
+	 */
 	@PermitAll
 	@GET
 	@Path("/id/{id}")
 	@Produces("application/json")
 	public Response getAlunoById(@PathParam("id") int idAluno) {
 		
+		// Preparando a resposta. Provisoriamente o sistema preparará a resposta como requisição incorreta.
 		ResponseBuilder builder = Response.status(Response.Status.BAD_REQUEST);
 		builder.expires(new Date());
 
 		try {
-
+			
+			// Regra de negócio e manipulação de dados nesse ponto.
 			Aluno aluno = AlunoDAO.getInstance().getById(idAluno); 
 			
 			if (aluno != null) {
 				
+				// As informaçãos associadas ao build para o response.
 				builder.status(Response.Status.OK);
 				builder.entity(aluno);
 				
 			} else {
 				
+				// Conteúdo não encontrado.
 				builder.status(Response.Status.NOT_FOUND);
 			}
 
@@ -82,6 +112,7 @@ public class AlunoController {
 			builder.status(Response.Status.INTERNAL_SERVER_ERROR);
 		}
 
+		// Resposta
 		return builder.build();
 	}
 }
