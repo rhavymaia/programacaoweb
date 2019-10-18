c0158147939f19da7b8a2570ff1359ccd8947287
ifpb <rmg3@cin.ufpe.br>
2019-10-18 09:49:38 -0300

    Validação com angularjs.

--- EscolaWebApp/js/app.js
+++ EscolaWebApp/js/app.js
@@ -1,10 +1,10 @@
 // Inicializar o módulo.
 let nomeApp = 'EscolaWebApp';
-let modulos = [];
+let modulos = ["ngMessages"];
 var app = angular.module(nomeApp, modulos);
 
 // Estrutura básica para uma função no controlador.
-var homeController = function($scope){
+var homeController = function($scope) {
   $scope.nome = "";
 
   $scope.desejarBoasVindas = function() {
@@ -15,13 +15,53 @@ var homeController = function($scope){
 
 app.controller('HomeController', homeController);
 
-var alunoController = function($scope){
+// Aluno - Controller
+var alunoController = function($scope) {
   $scope.nome = "";
   $scope.matricula = "";
   $scope.cpf = "";
   $scope.nascimento = "";
   $scope.id_endereço = "";
   $scope.id_endereço = "";
+
+  $scope.cadastrar = function() {
+    alunoApi.cadastrar($scope.aluno)
+      .then(function(response) {})
+      .catch(function(error) {});
+    $scope.alunoForm.$setPristine();
+  }
 }
 
 app.controller('AlunoController', alunoController);
+
+// Aluno - Factory
+var alunoFactory = function($http) {
+
+  var baseUrl = "localhost:5000";
+  var path = baseUrl + "/aluno";
+
+  var _cadastrar = function(aluno) {
+    return $http.post(_path, aluno)
+  };
+
+  var _atualizar = function(aluno) {
+    return $http.put(_path, aluno)
+  };
+
+  var _buscarPorId = function(id) {
+    return $http.get(_path + "/" + encodeURI(id))
+  };
+
+  var _listar = function() {
+    return $http.get(_path)
+  };
+
+  return {
+    cadastrar: _cadastrar,
+    atualizar: _atualizar,
+    buscarPorId: _buscarPorId,
+    listar: _listar
+  };
+}
+
+app.factory("alunoApi", alunoFactory);
