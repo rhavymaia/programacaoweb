c056b505e2c27a62d8fdeef9bb2d4556ad2a4c0f
ifpb <rmg3@cin.ufpe.br>
2019-11-20 09:10:18 -0300

    Submissão dos dados de cadastro para o serviço.

--- EscolaWebApp/js/app.js
+++ EscolaWebApp/js/app.js
@@ -19,9 +19,11 @@ app.controller('HomeController', homeController);
 var alunoController = function($scope, $mdToast, alunoApi) {
 
   $scope.aluno = {};
-  let aluno = $scope.aluno;
 
   $scope.cadastrar = function() {
+    // Criar uma cópia do aluno do $scope.
+    let aluno = angular.copy($scope.aluno);
+
     // Converter formato da data: brazilian -> american.
     var data = moment(aluno.nascimento, "DD/MM/YYYY");
     aluno.nascimento = data.format("YYYY-MM-DD");
@@ -35,10 +37,7 @@ var alunoController = function($scope, $mdToast, alunoApi) {
             .hideDelay(6000);
         $mdToast.show(toast);
 
-        delete $scope.aluno;
-        $scope.aluno = {};
-        $scope.alunoForm.$setPristine();
-        $scope.alunoForm.$setUntouched();
+        limparFormulario();
       })
       .catch(function(error) {
         var toast = $mdToast.simple()
@@ -48,7 +47,19 @@ var alunoController = function($scope, $mdToast, alunoApi) {
             .hideDelay(6000);
         $mdToast.show(toast);
       });
-  }
+  };
+
+  let limparFormulario = function () {
+
+        // Reinicializa a variável aluno.
+        angular.copy({}, $scope.aluno);
+
+        // Reinicializa o estado do campo para os eventos e validação.
+        // É necessário indicar o atributo name no formulário <form>
+        $scope.alunoForm.$setPristine();
+        $scope.alunoForm.$setUntouched();
+        $scope.alunoForm.$setValidity();
+    }
 }
 
 app.controller('AlunoController', alunoController);
