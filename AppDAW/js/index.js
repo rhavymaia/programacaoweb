e462a955584e738fd3a221b207c738dcc00a5051
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2021-03-02 10:00:31 -0300

    Aula Semana 19 - Funcões e Objetos

--- AppDAW/js/index.js
+++ AppDAW/js/index.js
@@ -1,36 +1,119 @@
-let idade = 18;
-let peso = 60;
-let altura = 1.89;
-
+// Semana 17
+var idade = 18;
+console.log(idade);
 const PI = 3.14;
+console.log(PI);
+//PI = 3.141516; Não pode reatribuir valor a uma constante.
+let peso = 60;
+console.log(peso);
 
-console.log("Sua idade é: " + idade);
+let nome = "João da Silva";
+let matriculado = true;
 
-function minhaFuncao(){}
-
-function outraFuncao(minhaFuncao){
-  // Callback
-  /*
-    Callback
-  */
-  minhaFuncao()
+// Estrutura de condição composto
+if (matriculado == true) {
+  console.log("Aluno(a) está matriculado(a)")
+} else {
+  console.log("Aluno(a) não está matriculado(a)")
 }
 
-for (var i = 0; i <10; i++){
+// Estrutura de condição com if composto, aninhado e encadeado.
+if (idade >= 18){
+  // condição verdadeira
+  console.log("Você é maior de idade.");
+} else {
+  // condição falsa
+  console.log("Você é menor de idade.");
+
+  if (idade >= 2 && idade <=10) {
+    console.log("e também é criança.")
+  } else if (idade > 0) {
+    console.log("e também é bebê.")
+  }
+}
+
+// Semana 18
+// Estruturas de repetição
+// while
+let i = 1;
+while (i <= 10) {
+  // comandos
   console.log(i);
+  i++;
 }
 
-let mangas = ["Tommy", "Rosa", "Espada"];
-for (let manga of mangas){
-  console.log(manga);
+// for com autoincremento
+console.log("for");
+for (let j = 1; j <= 10; j++) {
+  // comandos
+  console.log(j);
 }
 
+// for..in
+let frutas = ["banana", "manga", "jaca", "maçã"];
+for (let indice in frutas){
+  console.log(indice);
+}
+
+// for..of
+for (let fruta of frutas){
+  console.log(fruta);
+}
+
+// Objeto - JSON
 let pessoa = {
-  "nome": "José",
-  "nascimento": new Date(),
-  "altura": 1.80
+  "idade": 18,
+  "peso": 60,
+  "altura": 1.80,
+  "falar": function (){
+    console.log("Estou falando!");
+  }
+};
+pessoa.falar();
+var quadrado = (x) => x * x
+console.log(quadrado(2))
+
+// Semana 19
+// Função expressa
+let elevar = function (x, potencia) {
+  return x ** potencia;
+}
+// Função arrow
+elevar = (x, potencia) => x ** potencia;
+
+// Função declarativa
+function minhaFuncao () {
+  // comandos
+  console.log("Entrou na minha função");
+  for (var i = 0; i <= 3; i++) {
+    let resultado = elevar(i, 2)
+    //alert(resultado);
+  }
 }
 
-for (let p in pessoa){
-  console.log(pessoa[p]);
+minhaFuncao();
+
+let falar = function () {
+  console.log("Falar como eu queria!");
 }
+
+let pessoa1 = {
+  "nome": "João",
+  "falar": falar
+}
+
+pessoa1.falar();
+
+// Classe
+function Pessoa() {
+  this.nome = "";
+  let andar = function () {
+    console.log("Estou andando!");
+  }
+  this.andar = andar;
+}
+
+var p1 = new Pessoa();
+p1.nome = "Maria";
+console.log(p1.nome);
+p1.andar();
