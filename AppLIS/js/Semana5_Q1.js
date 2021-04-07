d16d271cfd05222ee8d05e3e5e03a7caee0f0abd
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2021-04-07 13:23:57 -0300

    Resolução da atividade da Semana 5.

--- AppLIS/js/Semana5_Q1.js
+++ AppLIS/js/Semana5_Q1.js
@@ -1,1 +1,14 @@
 // Semana 5 - Questão 1
+// Gerar uma lista em HTML com as tags <ul> e <li>.
+function createList(quantidadeLi, texto) {
+  console.log("criando a lista");
+  for(var i =0; i < quantidadeLi; i++) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(texto + " " + i);
+    node.appendChild(textnode);
+
+    document.getElementById("minhaLista").appendChild(node);
+  }
+}
+
+createList(20, "IFPB");
