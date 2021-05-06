7e068c99f5dd626f18d7396d9e2e237563d8035c
rhavymaia <rmg3@cin.ufpe.br>
2021-05-06 10:59:21 -0300

    Cadastrar novo item de comida na food-cafeteria.

--- food-cafeteria/js/main.js
+++ food-cafeteria/js/main.js
@@ -1,8 +1,8 @@
 import dataset from './model/dataset.js';
-import food from './model/food.js';
+import foodsModel from './model/food.js';
 
-food.load(dataset);
-let foods = food.readAll();
+foodsModel.load(dataset);
+let foods = foodsModel.readAll();
 
 for (const item of foods) {
   const itensDiv = document.getElementById('itens');
@@ -10,7 +10,7 @@ for (const item of foods) {
   itensDiv.insertAdjacentHTML('beforeend', cardHTML);
 }
 
-function addItem(item){
+function addItem(item) {
   let cardHTML = `
     <div class='col'>
       <div class='card' style='width: 18rem;'>
@@ -18,9 +18,30 @@ function addItem(item){
         <div class='card-body'>
           <h5 class='card-title'>${item.name}</h5>
           <p class='card-text'>${item.description}</p>
-          <a href='#' class='btn btn-primary'>Go somewhere</a>
+          <a href='#' class='btn btn-primary'>Adicionar</a>
         </div>
       </div>
     </div>`;
   return cardHTML;
 }
+
+const foodForm = document.getElementById('formFood');
+foodForm.onsubmit = function (event) {
+  // Previnir que o modal fique abrindo em loop.
+  event.preventDefault();
+
+  // Montar o objeto food baseado nos dados do formulário.
+  let food = Object.fromEntries(new FormData(formFood));
+
+  // Adicionar o item (food) no LocalStorage.
+  const newFood = foodsModel.create(food);
+
+  const itensDiv = document.getElementById('itens');
+  const cardHTML = addItem(newFood);
+  itensDiv.insertAdjacentHTML('beforeend', cardHTML);
+
+  // Fechar o foodModal
+  var myModalEl = document.getElementById('foodModal');
+  var foodModal = bootstrap.Modal.getInstance(myModalEl);
+  foodModal.toggle();
+};
--- food-cafeteria/js/model/dataset.js
+++ food-cafeteria/js/model/dataset.js
@@ -3,26 +3,30 @@ const dataset = [
     id: 1,
     name: 'Hambúrguer',
     image: 'images/hamburguer.jpg',
-    description: 'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado'
+    description:
+      'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
   },
   {
     id: 2,
     name: 'Sanduíche',
     image: 'images/sanduiche.jpg',
-    description: 'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.'
+    description:
+      'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
   },
   {
     id: 3,
     name: 'Café',
     image: 'images/cafe.jpg',
-    description: 'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.'
+    description:
+      'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
   },
   {
     id: 4,
     name: 'Suco',
     image: 'images/suco.jpg',
-    description: 'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.'
-  }
+    description:
+      'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
+  },
 ];
 
 export default dataset;
